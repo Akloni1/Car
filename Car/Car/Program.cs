d10f0874be8925908cd9b983f10a5395a4d0ba35
Akloni1 <61523151+Akloni1@users.noreply.github.com>
2022-03-09 20:18:13 +0300

    Изменение на грузовой автомобль

--- Car/Car/Program.cs
+++ Car/Car/Program.cs
@@ -10,7 +10,7 @@ namespace Car
             var truck = new Truck("Грузовой", 20, 100, 60, 600);
             var sportCar = new SportCar("Спортивный", 15, 50, 150);
             // здесь реализован полиморфизм, соответственно можно подставить любой объект автомобиля, и методы отработают
-            ICar car = passengerCar;
+            ICar car = truck;
             Console.WriteLine(car.TravelTime(180) + " ч.");
             Console.WriteLine(car.PowerReserve() + " км.");
             Console.WriteLine(car.PowerReserveLoadedCar() + " км.");
